ea86b3e30d9eea8826bd0717973807d954c022ae
github username <github email address>
2017-08-20 19:16:53 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,20 @@
+require 'pry'
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    first = self.first_name
+    last = self.last_name
+    first + " " + last
+  end
+
+  def list_roles
+    characters.collect do |character|
+      name = character.name
+      show = character.show.name
+      name + " - " + show
+    end
+  end
+
 end
